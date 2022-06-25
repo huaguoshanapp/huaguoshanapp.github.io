c77f31cdd16a04b2cecd669dcab9044ffded6188
anymarvel <314599558@qq.com>
2022-06-25 15:31:29 +0800

    升级Android版本

--- js/index.js
+++ js/index.js
@@ -257,7 +257,7 @@ if (USER_AGENT.match(/MSIE 8./) || USER_AGENT.match(/MSIE 7./) || USER_AGENT.mat
                 } else if (GLOBAL.DEVICE_TYPE === 2) {
                     ax.style.display = 'none';
                     ag.style.display = 'block';
-                    ah.href = 'https://gitee.com/dssljt/hgsdist/attach_files/1081014/download/huaguoshan.apk';
+                    ah.href = 'https://gitee.com/dssljt/hgsdist/attach_files/1107232/download/huaguoshan1.0.2.apk';
                 }
                 if (GLOBAL.DEVICE_TYPE) {
                     M.style.backgroundImage = 'url("images/mobile_function4_table_background_4x.png")';
